31e8c767659e26d826b4927759c3f5d7de230fb8
Gavin Morgan <morgan.gavin@gmail.com>
2018-09-24 12:07:17 -0700

    basic fake table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
+
+const data = [{
+    name: 'Tanner Linsley',
+    age: 26,
+    friend: {
+      name: 'Jason Maurer',
+      age: 23,
+    }
+  },{
+    name: 'Danny Spitzberg',
+    age: 36,
+    friend: {
+      name: 'Todd Lundgren',
+      age: 13,
+    }
+  }]
+
+  const columns = [{
+    Header: 'Name',
+    accessor: 'name' // String-based value accessors!
+  }, {
+    Header: 'Age',
+    accessor: 'age',
+    Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+  }, {
+    id: 'friendName', // Required because our accessor is not a string
+    Header: 'Friend Name',
+    accessor: d => d.friend.name // Custom value accessors!
+  }]
 
 class App extends Component {
   render() {
@@ -8,11 +39,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Tourney</h1>
         </header>
-        <p className="App-intro">
-          To get started, destroy <code>src/App.js</code> and save.
-        </p>
+
+        <ReactTable
+          data={data}
+          columns={columns}
+        />
       </div>
     );
   }
